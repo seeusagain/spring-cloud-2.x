e8588bc7f2fed40e99999e134d4f4a8b1f603533
ahuang <765110919@qq.com>
2020-04-09 11:41:52 +0800

    添加web项目

---
+++ microservice-web/src/main/java/com/java/ApplicationWeb.java
@@ -0,0 +1,25 @@
+package com.java;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplicationWeb {
+
+    static Logger logger = LoggerFactory.getLogger(ApplicationWeb.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationWeb.class, args);
+
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        logger.info("----microservice-web started-----");
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+    }
+
+}
